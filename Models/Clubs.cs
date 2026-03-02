3efe44382743f0b4993fa6a73afba2dc429545ff
Shahzod0079 <toshmatovshahzod69@gmail.com>
2026-03-02 16:18:02 +0500

    Разработанные модели. Тошматов

---
+++ Models/Clubs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_26_Toshmatov.Models
+{
+    internal class Clubs
+    {
+        public int ID { get; set; } 
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public String WorkTime { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_26_Toshmatov.Models
+{
+    internal class Users
+    {
+        public int Id { get; set; }
+
+        public string FIO { get; set; }
+
+        public DateTime RenStart { get; set; }
+
+        public int Duration { get; set; }
+
+        public string Idclub { get; set; }
+
+    }
+}
