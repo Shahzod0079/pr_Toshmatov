17c704864acb2ee090f6bdbc85f978ff5232d789
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-04 09:22:56 +0500

    Разработанный модуль пользователей. Тошматов

---
+++ Pages/Users/Add.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using pr_26_Toshmatov.Classes;
+
+namespace pr_26_Toshmatov.Pages.Users
+{
+    public partial class Add : Page
+    {
+        public ClubsContext AllClub = new ClubsContext();
+        public Models.Users User;
+        public Main Main;
+
+        public Add(Main Main, Models.Users User = null)
+        {
+            InitializeComponent();
+
+            this.Main = Main;
+
+            Clubs.Items.Add("Выберите ...");
+
+            foreach (Models.Clubs Club in AllClub.Clubs)
+            {
+                Clubs.Items.Add(Club.Name);
+            }
+
+            if (User != null)
+            {
+                this.User = User;
+                this.FIO.Text = User.FIO;
+                this.RentDate.Text = User.RentStart.ToString("yyyy-MM-dd");
+                this.RentTime.Text = User.RentStart.ToString("HH:mm");
+                this.Duration.Text = User.Duration.ToString();
+
+                var clubName = AllClub.Clubs.Where(x => x.Id == User.IdClub).First().Name;
+                Clubs.SelectedItem = clubName;
+
+                BtnSave.Content = "Изменить";
+            }
+        }
+
+        private void SaveUser(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DateTime DTRentStart = new DateTime();
+            DateTime.TryParse(this.RentDate.Text, out DTRentStart);
+            DTRentStart = DTRentStart.Add(TimeSpan.Parse(this.RentTime.Text));
+
+            if (this.User == null)
+            {
+                User = new Models.Users();
+                User.FIO = this.FIO.Text;
+                User.RentStart = DTRentStart;
+                User.Duration = Convert.ToInt32(this.Duration.Text);
+                User.IdClub = AllClub.Clubs.Where(x => x.Name == Clubs.SelectedItem).First().Id;
+
+                this.Main.AllUsers.Users.Add(this.User);
+            }
+            else
+            {
+                User.FIO = this.FIO.Text;
+                User.RentStart = DTRentStart;
+                User.Duration = Convert.ToInt32(this.Duration.Text);
+                User.IdClub = AllClub.Clubs.Where(x => x.Name == Clubs.SelectedItem).First().Id;
+            }
+
+            this.Main.AllUsers.SaveChanges();
+
+            MainWindow.init.OpenPages(new Pages.Users.Main());
+        }
+
+        private void Cancel(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(new Pages.Users.Main());
+        }
+    }
+}
---
+++ Pages/Users/Elemets/Item.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using pr_26_Toshmatov.Classes;
+
+namespace pr_26_Toshmatov.Pages.Users.Elements
+{
+    public partial class Item : UserControl
+    {
+        public ClubsContext AllClub = new ClubsContext();
+        Main Main;
+        Models.Users User;
+
+        public Item(Models.Users User, Main Main)
+        {
+            InitializeComponent();
+
+            this.FIO.Text = User.FIO;
+            this.RentDate.Text = User.RentStart.ToString("yyyy-MM-dd");
+            this.RentTime.Text = User.RentStart.ToString("HH:mm");
+            this.Duration.Text = User.Duration.ToString();
+
+            var club = AllClub.Clubs.Where(x => x.Id == User.IdClub).FirstOrDefault();
+            if (club != null)
+            {
+                this.Club.Text = club.Name;
+            }
+            else
+            {
+                this.Club.Text = "Клуб не найден";
+            }
+
+            this.Main = Main;
+            this.User = User;
+        }
+
+        private void EditUser(object sender, System.Windows.RoutedEventArgs e) =>
+            MainWindow.init.OpenPages(new Pages.Users.Add(this.Main, this.User));
+
+        private void DeleteUser(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Main.AllUsers.Users.Remove(User);
+            Main.AllUsers.SaveChanges();
+            Main.Parent.Children.Remove(this);
+        }
+    }
+}
